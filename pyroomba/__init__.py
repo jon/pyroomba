6b597a8c5c5127581c3799cd9f490c5f90e06a38
Jon Olson <jon@damogran.com>
2009-02-18 05:51:49 -0500

    Fixed docstring on __init__.py to actually be a docstring

--- pyroomba/__init__.py
+++ pyroomba/__init__.py
@@ -1,9 +1,4 @@
-from roomba import *
-from roomba_dynamics import *
-from events import *
-import sensors
-
-"""pyroomba -- A library for controlling iRobot devices.
+"""A library for controlling iRobot devices.
 
 As the name implies, this package is primarily intended for controlling and
 monitoring iRobot Roomba series robots. The Roomba class provides a mostly
@@ -12,4 +7,10 @@ convenient set of python methods. EventLoop provides a convenient way to
 handle sensor data returned from the robot in a sane manner, and 
 RoombaDynamics provides an attempt at reasonable localization based on the
 robot's built-in odometry.
+
 """
+
+from roomba import *
+from roomba_dynamics import *
+from events import *
+import sensors
