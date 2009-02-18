901134e71826dbf228d5b5544f1bb0944027555d
Jon Olson <jon@damogran.com>
2009-02-18 03:32:49 -0500

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = "0.1",
     packages = find_packages(),
     
-    install_requres = ['pyserial>=2.0'],
+    install_requires = ['pyserial>=2.0'],
     
     author = "Jon Olson",
     author_email = "jon@damogran.com",
